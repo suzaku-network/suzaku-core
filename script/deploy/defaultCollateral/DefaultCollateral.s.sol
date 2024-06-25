1456208ac5408ddc1119270e290837955edf8ff4
Gauthier Leonard <gauthier@e36knots.com>
2024-06-25 20:21:52 +0200

    feat: add Symbiotic collateral code

---
+++ script/deploy/defaultCollateral/DefaultCollateral.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+
+import {IDefaultCollateralFactory} from
+    "src/interfaces/defaultCollateral/IDefaultCollateralFactory.sol";
+
+contract DefaultCollateralScript is Script {
+    function run(
+        address defaultCollateralFactory,
+        address asset,
+        uint256 initialLimit,
+        address limitIncreaser
+    ) external {
+        vm.startBroadcast();
+
+        IDefaultCollateralFactory(defaultCollateralFactory).create(
+            asset, initialLimit, limitIncreaser
+        );
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/mocks/PermitToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2024 Symbiotic
+pragma solidity 0.8.25;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract PermitToken is ERC20, ERC20Permit {
+    constructor(string memory name_) ERC20(name_, "") ERC20Permit(name_) {
+        _mint(msg.sender, 1_000_000 * 1e18);
+    }
+}
