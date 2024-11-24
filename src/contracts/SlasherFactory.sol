cef01a7a1ad8e32947823668d8ff7769828fbaab
GENGE <gonzaloetjo@gmail.com>
2024-11-24 19:57:38 +0100

    feat: delegator & slasher factories

---
+++ src/contracts/SlasherFactory.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import { Entity } from "./common/Entity.sol";
+import { IEntity } from "../interfaces/common/IEntity.sol";
+import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+
+import { ISlasherFactory } from "../interfaces/ISlasherFactory.sol";
+
+contract SlasherFactory is ISlasherFactory, Ownable, ERC165 {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using Clones for address;
+    using ERC165Checker for address;
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    mapping(uint64 => bool) public blacklisted;
+
+    EnumerableSet.AddressSet private _whitelistedImplementations;
+    EnumerableSet.AddressSet private _entities;
+    
+    modifier checkType(uint64 type_) {
+        if (type_ >= totalTypes()) {
+            revert InvalidType();
+        }
+        _;
+    }
+
+    constructor(
+        address owner_
+    ) Ownable(owner_) {}
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function totalTypes() public view returns (uint64) {
+        return uint64(_whitelistedImplementations.length());
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function implementation(uint64 type_) public view returns (address) {
+        return _whitelistedImplementations.at(type_);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function whitelist(address implementation_) external onlyOwner {
+        // Check if the implementation supports the IEntity interface via ERC165
+        if (!implementation_.supportsInterface(type(IEntity).interfaceId)) {
+            revert InvalidImplementation();
+        }
+
+        if (IEntity(implementation_).FACTORY() != address(this) || IEntity(implementation_).TYPE() != totalTypes()) {
+            revert InvalidImplementation();
+        }
+        if (!_whitelistedImplementations.add(implementation_)) {
+            revert AlreadyWhitelisted();
+        }
+
+        emit Whitelist(implementation_);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function blacklist(uint64 type_) external onlyOwner checkType(type_) {
+        if (blacklisted[type_]) {
+            revert AlreadyBlacklisted();
+        }
+
+        blacklisted[type_] = true;
+
+        emit Blacklist(type_);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function create(uint64 type_, bytes calldata data) external returns (address entity_) {
+        entity_ = implementation(type_).cloneDeterministic(keccak256(abi.encode(totalEntities(), type_, data)));
+
+        _addSlasherEntity(entity_);
+
+        IEntity(entity_).initialize(data);
+    }
+
+    // function create(uint64 type_, bytes calldata data) external returns (address _slasher) {
+    //     _slasher = implementation(type_).cloneDeterministic(keccak256(abi.encode(type_, data)));
+
+    //     IEntity(_slasher).initialize(data);
+    // }
+
+    function _addSlasherEntity(
+        address entity_
+    ) internal {
+        _entities.add(entity_);
+
+        emit AddEntity(entity_);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function entity(
+        uint256 index
+    ) public view returns (address) {
+        return _entities.at(index);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function isEntity(
+        address entity_
+    ) public view returns (bool) {
+        return _entities.contains(entity_);
+    }
+
+    /**
+     * @inheritdoc ISlasherFactory
+     */
+    function totalEntities() public view returns (uint256) {
+        return _entities.length();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC165, ISlasherFactory) returns (bool) {
+        return interfaceId == type(ISlasherFactory).interfaceId || super.supportsInterface(interfaceId);
+    }
+}
--- src/interfaces/ISlasherFactory.sol
+++ src/interfaces/ISlasherFactory.sol
@@ -1,6 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IFactory} from "./IFactory.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-interface ISlasherFactory is IFactory {}
+/**
+ * @title ISlasherFactory
+ * @dev Combined interface integrating ISlasherFactory, IFactory, IRegistry, and IERC165.
+ */
+interface ISlasherFactory is IERC165 {
+    error EntityNotExist();
+    error AlreadyBlacklisted();
+    error AlreadyWhitelisted();
+    error InvalidImplementation();
+    error InvalidType();
+
+    /**
+     * @notice Emitted when an entity is added.
+     * @param entity address of the added entity
+     */
+    event AddEntity(address indexed entity);
+
+    /**
+     * @notice Emitted when a new type is whitelisted.
+     * @param implementation address of the new implementation
+     */
+    event Whitelist(address indexed implementation);
+
+    /**
+     * @notice Emitted when a type is blacklisted (e.g., in case of invalid implementation).
+     * @param type_ type that was blacklisted
+     * @dev The given type is still deployable.
+     */
+    event Blacklist(uint64 indexed type_);
+
+    /**
+     * @notice Query if a contract implements an interface
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     * @return `true` if the contract implements `interfaceId` and
+     *  `interfaceId` is not 0xffffffff, `false` otherwise
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+    /**
+     * @notice Get if a given address is an entity.
+     * @param account address to check
+     * @return if the given address is an entity
+     */
+    function isEntity(address account) external view returns (bool);
+
+    /**
+     * @notice Get a total number of entities.
+     * @return total number of entities added
+     */
+    function totalEntities() external view returns (uint256);
+
+    /**
+     * @notice Get an entity given its index.
+     * @param index index of the entity to get
+     * @return address of the entity
+     */
+    function entity(uint256 index) external view returns (address);
+
+    /**
+     * @notice Get the total number of whitelisted types.
+     * @return total number of types
+     */
+    function totalTypes() external view returns (uint64);
+
+    /**
+     * @notice Get the implementation for a given type.
+     * @param type_ position to get the implementation at
+     * @return address of the implementation
+     */
+    function implementation(uint64 type_) external view returns (address);
+
+    /**
+     * @notice Get if a type is blacklisted (e.g., in case of invalid implementation).
+     * @param type_ type to check
+     * @return whether the type is blacklisted
+     * @dev The given type is still deployable.
+     */
+    function blacklisted(uint64 type_) external view returns (bool);
+
+    /**
+     * @notice Whitelist a new type of entity.
+     * @param implementation address of the new implementation
+     */
+    function whitelist(address implementation) external;
+
+    /**
+     * @notice Blacklist a type of entity.
+     * @param type_ type to blacklist
+     * @dev The given type will still be deployable.
+     */
+    function blacklist(uint64 type_) external;
+
+    /**
+     * @notice Create a new entity at the factory.
+     * @param type_ type's implementation to use
+     * @param data initial data for the entity creation
+     * @return address of the entity
+     * @dev CREATE2 salt is constructed from the given parameters.
+     */
+    function create(uint64 type_, bytes calldata data) external returns (address);
+}
